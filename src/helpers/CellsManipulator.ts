babb26488894459504a47ae94a9f1c9c71ca010f
Nikita Ovchinnikov <nickovchinnikof@gmail.com>
2021-08-03 16:14:41 +0300

    CellsManipulator is done

--- src/helpers/CellsManipulator.ts
+++ src/helpers/CellsManipulator.ts
@@ -50,6 +50,47 @@ export const incrementNeibours = (coords: Coords, field: Field): Field => {
 };
 
 /**
+ * Detect solved puzzle based on the player and game fields coorelation
+ * @param playerField
+ * @param gameField
+ * @returns
+ */
+export const detectSolvedPuzzle = (
+  playerField: Field,
+  gameField: Field
+): Field => {
+  const { hidden, bomb, flag } = CellState;
+
+  let bombsCounter = 0;
+  let detectedBombsCounter = 0;
+  let hiddenCells = 0;
+
+  for (const y of gameField.keys()) {
+    for (const x of gameField[y].keys()) {
+      const gameCell = gameField[y][x];
+      const playerCell = playerField[y][x];
+
+      if (playerCell === hidden) {
+        hiddenCells++;
+      }
+
+      if (gameCell === bomb) {
+        bombsCounter++;
+
+        if (playerCell === flag) {
+          detectedBombsCounter++;
+        }
+      }
+    }
+  }
+
+  const solvedPuzzle =
+    bombsCounter === detectedBombsCounter && hiddenCells === 0;
+
+  return solvedPuzzle ? gameField : playerField;
+};
+
+/**
  * Open cell in the player field using game field info
  * @param {Coords} coords
  * @param {Field} playerField
@@ -77,15 +118,10 @@ export const openCell = (
 
     for (const [y, x] of Object.values(items)) {
       if (checkItemInField([y, x], gameField)) {
-        const gameCell = gameField[y][x];
         const playerCell = playerField[y][x];
 
-        if (gameCell === empty && playerCell === hidden) {
+        if (playerCell === hidden) {
           playerField = openCell([y, x], playerField, gameField);
-        }
-
-        if (gameCell < bomb) {
-          playerField[y][x] = gameCell;
         }
       }
     }
@@ -93,5 +129,36 @@ export const openCell = (
 
   playerField[y][x] = gameCell;
 
-  return playerField;
+  return detectSolvedPuzzle(playerField, gameField);
+};
+
+/**
+ * Set flag to the cell
+ * @param {Coords} coords
+ * @param {Field} playerField
+ * @param {Field} gameField
+ * @returns {Field}
+ */
+export const setFlag = (
+  coords: Coords,
+  playerField: Field,
+  gameField: Field
+): Field => {
+  const [y, x] = coords;
+  const cell = playerField[y][x];
+
+  const { flag, weakFlag, hidden } = CellState;
+
+  switch (cell) {
+    case flag:
+      playerField[y][x] = weakFlag;
+      break;
+    case weakFlag:
+      playerField[y][x] = hidden;
+      break;
+    case hidden:
+      playerField[y][x] = flag;
+  }
+
+  return detectSolvedPuzzle(playerField, gameField);
 };

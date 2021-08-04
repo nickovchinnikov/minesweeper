c008a142eeb46cd9cad63971819b562de81a65b4
Nikita Ovchinnikov <nickovchinnikof@gmail.com>
2021-08-04 15:17:44 +0300

    Refactoring test cases for GameWithHooks

--- src/modules/GameWithHooks/GameWithHooks.test.tsx
+++ src/modules/GameWithHooks/GameWithHooks.test.tsx
@@ -125,8 +125,107 @@ describe('Dynamic game with react-hooks', () => {
 
     expect(asFragment()).toMatchSnapshot();
   });
+  it('Check bomb counter', () => {
+    render(<GameWithHooks defaultField={defautGameField} />);
+
+    userEvent.click(screen.getByTestId('2,2'));
+    userEvent.click(screen.getByTestId('4,4'));
+
+    userEvent.click(screen.getByTestId('0,0'), { button: 2 });
+    userEvent.click(screen.getByTestId('0,1'), { button: 2 });
+
+    expect(screen.getByText(8)).toBeInTheDocument();
+  });
+  it('Check timer has started by click', async () => {
+    jest.useFakeTimers();
+
+    render(<GameWithHooks defaultField={defautGameField} />);
+
+    const timeShouldBePassed = 5;
+
+    for (let i = 0; i < timeShouldBePassed; i++) {
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+    }
+
+    // Timer shouldn't works before game has started
+    expect(screen.queryByText(timeShouldBePassed)).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByTestId('4,4'));
+
+    for (let i = 0; i < timeShouldBePassed; i++) {
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+    }
+
+    expect(await screen.findByText(timeShouldBePassed)).toBeInTheDocument();
+  });
+  it('Check timer shpuld has started by mark a cell by flag', async () => {
+    jest.useFakeTimers();
+
+    render(<GameWithHooks defaultField={defautGameField} />);
+
+    const timeShouldBePassed = 5;
+
+    for (let i = 0; i < timeShouldBePassed; i++) {
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+    }
+
+    // Timer shouldn't works before game has started
+    expect(screen.queryByText(timeShouldBePassed)).not.toBeInTheDocument();
+
+    userEvent.click(screen.getByTestId('0,0'), { button: 2 });
+
+    for (let i = 0; i < timeShouldBePassed; i++) {
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+    }
+
+    expect(await screen.findByText(timeShouldBePassed)).toBeInTheDocument();
+  });
+  it('Loose scenario', async () => {
+    jest.useFakeTimers();
+
+    const { asFragment } = render(
+      <GameWithHooks defaultField={defautGameField} />
+    );
+
+    userEvent.click(screen.getByTestId('4,4'));
+
+    const timeShouldBePassed = 5;
+
+    for (let i = 0; i < timeShouldBePassed; i++) {
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+    }
+
+    expect(await screen.findByText(timeShouldBePassed)).toBeInTheDocument();
+
+    userEvent.click(screen.getByTestId('0,0'));
+
+    expect(screen.getByText('🙁')).toBeInTheDocument();
+
+    for (let i = 0; i < timeShouldBePassed; i++) {
+      act(() => {
+        jest.advanceTimersByTime(1000);
+      });
+    }
+
+    // After loose time shoudn't change
+    expect(await screen.findByText(timeShouldBePassed)).toBeInTheDocument();
+
+    expect(asFragment()).toMatchSnapshot();
+  });
   it('Win scenario', () => {
     render(<GameWithHooks defaultField={defautGameField} />);
+
+    expect(screen.queryByText('😎')).not.toBeInTheDocument();
 
     userEvent.click(screen.getByTestId('0,0'), { button: 2 });
     userEvent.click(screen.getByTestId('0,1'), { button: 2 });
@@ -140,6 +239,8 @@ describe('Dynamic game with react-hooks', () => {
   it('Win scenario2', () => {
     render(<GameWithHooks defaultField={defautGameField} />);
 
+    expect(screen.queryByText('😎')).not.toBeInTheDocument();
+
     userEvent.click(screen.getByTestId('2,2'));
     userEvent.click(screen.getByTestId('4,4'));
 
@@ -149,29 +250,5 @@ describe('Dynamic game with react-hooks', () => {
     userEvent.click(screen.getByTestId('3,4'), { button: 2 });
 
     expect(screen.getByText('😎')).toBeInTheDocument();
-  });
-  it('Loose scenario', () => {
-    render(<GameWithHooks defaultField={defautGameField} />);
-
-    userEvent.click(screen.getByTestId('0,0'));
-
-    expect(screen.getByText('🙁')).toBeInTheDocument();
-  });
-  it('Check timer has started', async () => {
-    jest.useFakeTimers();
-
-    render(<GameWithHooks defaultField={defautGameField} />);
-
-    userEvent.click(screen.getByTestId('4,4'));
-
-    const timeShouldBePassed = 5;
-
-    for (let i = 0; i < timeShouldBePassed; i++) {
-      act(() => {
-        jest.advanceTimersByTime(1000);
-      });
-    }
-
-    expect(await screen.findByText(timeShouldBePassed)).toBeInTheDocument();
   });
 });

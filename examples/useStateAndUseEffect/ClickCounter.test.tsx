4a00bf026589ee38ecf8b26d654dcdf83ff6e821
NickOvchinnikov <nickovchinnikof@gmail.com>
2021-09-23 15:37:36 +0300

    Add useEffect example

--- examples/useStateAndUseEffect/ClickCounter.test.tsx
+++ examples/useStateAndUseEffect/ClickCounter.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 
 import { ClickCounter } from './ClickCounter';
 
 describe('Test ClickCounter', () => {
   it('should inc/dec counter when buttons inc/dec clicked', () => {
-    render(<ClickCounter />);
+    const mockHandler = jest.fn();
+
+    render(<ClickCounter documentClickHandler={mockHandler} />);
 
     const counter = screen.getByRole('heading');
     const increaseBtn = screen.getByRole('button', { name: 'Increase' });
@@ -24,5 +26,37 @@ describe('Test ClickCounter', () => {
 
     fireEvent.click(decreaseBtn);
     expect(counter.textContent).toBe('Counter: 0');
+  });
+  it('should update title when inc/dec button clicked', async () => {
+    const mockHandler = jest.fn();
+
+    const { unmount } = render(
+      <ClickCounter documentClickHandler={mockHandler} />
+    );
+
+    const increaseBtn = screen.getByRole('button', { name: 'Increase' });
+    const decreaseBtn = screen.getByRole('button', { name: 'Decrease' });
+
+    await waitFor(() => expect(document.title).toEqual('Counter: 0'));
+
+    fireEvent.click(increaseBtn);
+    await waitFor(() => expect(document.title).toEqual('Counter: 1'));
+    expect(mockHandler).toHaveBeenCalledTimes(1);
+
+    fireEvent.click(increaseBtn);
+    await waitFor(() => expect(document.title).toEqual('Counter: 2'));
+    expect(mockHandler).toHaveBeenCalledTimes(2);
+
+    fireEvent.click(decreaseBtn);
+    await waitFor(() => expect(document.title).toEqual('Counter: 1'));
+    expect(mockHandler).toHaveBeenCalledTimes(3);
+
+    fireEvent.click(document);
+    expect(mockHandler).toHaveBeenCalledTimes(4);
+
+    unmount();
+
+    fireEvent.click(document);
+    expect(mockHandler).toHaveBeenCalledTimes(4);
   });
 });

7e51b8c9e73348fdff6d2a6f9ad27d5d11db240e
Nikita Ovchinnikov <nickovchinnikof@gmail.com>
2022-02-07 12:39:35 +0400

    Add script lint Add eslint-plugin-storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
